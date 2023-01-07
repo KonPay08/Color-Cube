1d4e04935a8bce93b4bd39b97a1e77de4f73cc63
KonPay08 <ykh.821.family@gmail.com>
2023-01-07 13:53:33 +0900

    Transition to Next.js

--- src/components/View.container.tsx
+++
@@ -1,9 +0,0 @@
-import { useSurface } from "./useSurface"
-import { ViewColorTemplate } from "./View.template";
-
-export const View = () => {
-  const props = useSurface()
-  return (
-    <ViewColorTemplate {...props} />
-  );
-};
