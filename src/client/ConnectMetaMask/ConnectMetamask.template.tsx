5a6f6cad3fba91e06973e02e05b568f7ab8fea67
KonPay08 <ykh.821.family@gmail.com>
2023-01-11 01:21:09 +0900

    Add Authentication function

--- src/client/ConnectMetaMask/ConnectMetamask.template.tsx
+++ src/client/ConnectMetaMask/ConnectMetamask.template.tsx
@@ -2,7 +2,7 @@ import { ActivateBrowserWallet } from "@usedapp/core/dist/esm/src/providers"
 import { BigNumber } from "ethers"
 import { NextRouter } from "next/router"
 import { formatEther } from "@ethersproject/units";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Button } from "@chakra-ui/react";
 import { ChakraButtonTemplate } from "../../presentation/ChakraButton/ChakraButton.template";
 import { MouseEvent } from "react";
 import { css } from "@emotion/react";
@@ -12,9 +12,12 @@ type ConnectMetamaskTemplateProps = {
   account: string,
   router: NextRouter,
   etherBalance: BigNumber,
+  handleClick(): Promise<void>,
+  authenticated: boolean,
 };
 
 export const ConnectMetamaskTemplate: React.FC<ConnectMetamaskTemplateProps> = props => {
+
   const handleConnectWallet = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     props.activateBrowserWallet();
@@ -31,6 +34,16 @@ export const ConnectMetamaskTemplate: React.FC<ConnectMetamaskTemplateProps> = p
             <Text color="black" fontSize="md">
               {props.etherBalance && parseFloat(formatEther(props.etherBalance)).toFixed(5)} ETH
             </Text>
+            <Button
+              onClick={props.handleClick}
+            >
+              Show the limited contents?
+            </Button>
+            {props.authenticated && 
+              <Text fontSize="2xl" fontWeight="600">
+                You are a special user!
+              </Text>
+            }
             <ChakraButtonTemplate 
               label="Play"
               onClickHandler={handlePostRouter}
